af81da09061608c7d600cc7168c2268ba6380f2f
iheboueslati909 <hooba500@gmail.com>
2025-04-24 20:42:43 +0100

    added side menu navigation + pdf button included + some fixes

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,15 +1,17 @@
 class ReportsController < ApplicationController
-    def show
-      @report_data = JSON.parse(File.read(Rails.root.join("data", "sample_data.json")), symbolize_names: true)
-  
-      respond_to do |format|
-        format.html
-        format.pdf do
-          render pdf: "recon-report",
-                 template: "reports/show.html.erb",
-                 layout: "pdf"
-        end
+  def show
+    @report_data = JSON.parse(File.read(Rails.root.join("data", "sample_data.json")), symbolize_names: true)
+
+    respond_to do |format|
+      format.html do
+        @is_pdf = false
+      end
+      format.pdf do
+        @is_pdf = true
+        render pdf: "recon-report",
+               template: "reports/show.html.erb",
+               layout: "pdf"
       end
     end
   end
-  
+end
